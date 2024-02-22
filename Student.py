1b4125115b6494e12b5a2fd10e831c87433a65b7
Hiren Kuvadiya <hkuvadiya@gmail.com>
2024-02-22 14:00:28 +0530

    Initial commit

---
+++ Student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, student_class, age, place=None):
+        self.name = name
+        self.student_class = student_class
+        self.age = age
+        self.place = place
